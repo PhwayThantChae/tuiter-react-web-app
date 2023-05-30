8bf5aed8ff060c3989c2067e4e07822b0d4f85b0
Phway Thant Chae <ilovejazz555@gmail.com>
2023-05-30 01:38:24 -0400

    wip - a3 lab

--- src/labs/a3/java-script.js
+++ src/labs/a3/java-script.js
@@ -1,5 +1,13 @@
 import VariablesAndConstants from "./variables-and-constants";
 import VariableTypes from "./variable-types";
+import BooleanVariables from "./boolean-variables";
+import IfElse from "./if-else";
+import TernaryOperator from "./ternary-operator";
+import WorkingWithFunctions from "./working-with-functions";
+import WorkingWithArrays from "./working-with-arrays";
+import TemplateLiterals from "./template-literals";
+import House from "./house";
+import Spread from "./spread";
 
 function JavaScript() {
     console.log('Hello World');
@@ -8,6 +16,14 @@ function JavaScript() {
             <h1>JavaScript</h1>
             <VariablesAndConstants/>
             <VariableTypes/>
+            <BooleanVariables/>
+            <IfElse/>
+            <TernaryOperator/>
+            <WorkingWithFunctions/>
+            <WorkingWithArrays/>
+            <TemplateLiterals/>
+            <House/>
+            <Spread/>
         </div>
     );
 }

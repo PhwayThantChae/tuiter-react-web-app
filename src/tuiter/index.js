4331a704ce4323a167370c001042c271c925db6e
Phway Thant Chae <ilovejazz555@gmail.com>
2023-06-06 11:24:21 -0400

    done - a4 - 3.1

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -6,27 +6,33 @@ import ExploreScreen from "./explore-screen/index";
 import BookmarksScreen from "./bookmarks-screen";
 import ProfileScreen from "./profile-screen";
 import WhoToFollowList from "./who-to-follow-list";
+import whoReducer from "./reducers/who-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+const store = configureStore({ reducer: { who: whoReducer } });
 
 function Tuiter() {
   return (
     <div>
-      <Nav />
-      <div className="row">
-        <div className="col-xl-2 col-lg-1 col-md-2 col-sm-2 col-3">
-          <NavigationSidebar />
+      <Provider store={store}>
+        <Nav />
+        <div className="row">
+          <div className="col-xl-2 col-lg-1 col-md-2 col-sm-2 col-3">
+            <NavigationSidebar />
+          </div>
+          <div className="col-xl-7 col-lg-8 col-md-10 col-sm-10 col-9">
+            <Routes>
+              <Route path="/home" element={<HomeScreen />} />
+              <Route path="/explore" element={<ExploreScreen />} />
+              <Route path="/bookmarks" element={<BookmarksScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+            </Routes>
+          </div>
+          <div className="d-none d-lg-block d-xl-block d-xxl-block col-xl-3 col-lg-3">
+            <WhoToFollowList />
+          </div>
         </div>
-        <div className="col-xl-7 col-lg-8 col-md-10 col-sm-10 col-9">
-          <Routes>
-            <Route path="/home" element={<HomeScreen />} />
-            <Route path="/explore" element={<ExploreScreen />} />
-            <Route path="/bookmarks" element={<BookmarksScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-          </Routes>
-        </div>
-        <div className="d-none d-lg-block d-xl-block d-xxl-block col-xl-3 col-lg-3">
-            <WhoToFollowList/>
-        </div>
-      </div>
+      </Provider>
     </div>
   );
 }
